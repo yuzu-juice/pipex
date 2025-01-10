3af40acfeee7de3f015b9971f8db7829435bc307
yuzu-juice <yuzujuicesec@gmail.com>
2025-01-10 10:17:31 +0900

    change: cmd[0] to be rel path

--- src/split_cmds.c
+++ src/split_cmds.c
@@ -35,13 +35,13 @@ static void	append_cmd(char *str, t_cmd *cmd_list, char *envp[])
 	{
 		cmd_list->cmd = splited;
 		cmd_list->abs_path = get_abs_path(splited[0], envp);
-		cmd_list->cmd[0] = cmd_list->abs_path;
+		// cmd_list->cmd[0] = cmd_list->abs_path;
 		return ;
 	}
 	new_cmd = malloc(sizeof(t_cmd));
 	new_cmd->cmd = splited;
 	new_cmd->abs_path = get_abs_path(splited[0], envp);
-	new_cmd->cmd[0] = new_cmd->abs_path;
+	// new_cmd->cmd[0] = new_cmd->abs_path;
 	new_cmd->next = NULL;
 	last = get_last_cmd(cmd_list);
 	last->next = new_cmd;
