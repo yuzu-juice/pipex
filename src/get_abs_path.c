2ea42b60d3b2f2c9b10307729223f44875554428
yuzu-juice <yuzujuicesec@gmail.com>
2025-02-03 21:03:40 +0900

    fix: lint

--- src/get_abs_path.c
+++ src/get_abs_path.c
@@ -12,20 +12,11 @@
 
 #include "../include/pipex.h"
 
-char	*get_abs_path(char *command, char *envp[])
+static char	**get_paths_from_env(char *envp[])
 {
-	size_t	i;
-	size_t	path_len;
-	char	**paths;
-	char	*absolute_path;
-
-	if (!command)
-		return (NULL);
-	if (access(command, X_OK) == 0)
-		return (ft_strdup(command));
+	size_t	i = 0;
 	if (!envp)
 		return (NULL);
-	i = 0;
 	while (envp[i])
 	{
 		if (ft_strncmp("PATH=", envp[i], 5) == 0)
@@ -34,27 +25,52 @@ char	*get_abs_path(char *command, char *envp[])
 	}
 	if (!envp[i])
 		return (NULL);
-	paths = ft_split(envp[i] + ft_strlen("PATH="), ':');
+	return (ft_split(envp[i] + ft_strlen("PATH="), ':'));
+}
+
+static char	*build_executable_path(char *path, char *command)
+{
+	size_t	path_len;
+	char	*absolute_path;
+
+	path_len = ft_strlen(path) + ft_strlen(command) + 2;
+	absolute_path = ft_calloc(sizeof(char), path_len);
+	if (!absolute_path)
+		return (NULL);
+	ft_strlcpy(absolute_path, path, path_len);
+	ft_strlcat(absolute_path, "/", path_len);
+	ft_strlcat(absolute_path, command, path_len);
+	return (absolute_path);
+}
+
+char	*get_abs_path(char *command, char *envp[])
+{
+	char	**paths;
+	char	*absolute_path;
+	size_t	i;
+
+	if (!command)
+		return (NULL);
+	if (access(command, X_OK) == 0)
+		return (ft_strdup(command));
+	paths = get_paths_from_env(envp);
 	if (paths == NULL)
 		return (NULL);
 	i = 0;
 	while (paths[i])
 	{
-		path_len = ft_strlen(paths[i]) + ft_strlen(command) + 2;
-		absolute_path = ft_calloc(sizeof(char), path_len);
+		absolute_path = build_executable_path(paths[i], command);
 		if (!absolute_path)
 		{
 			free_string_array(paths);
 			return (NULL);
 		}
-		ft_strlcpy(absolute_path, paths[i], path_len);
-		ft_strlcat(absolute_path, "/", path_len);
-		ft_strlcat(absolute_path, command, path_len);
 		if (access(absolute_path, X_OK) == 0)
 		{
 			free_string_array(paths);
 			return (absolute_path);
 		}
+		free(absolute_path);
 		i++;
 	}
 	free_string_array(paths);
